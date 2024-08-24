f58b691cd42f4f211c0295088a37f882cd3f76fe
jyotir07 <jyotir.aditya.tf@gmail.com>
2024-08-24 12:35:11 +0530

    added numbers half pyramid function

--- patterns.cpp
+++ patterns.cpp
@@ -28,6 +28,16 @@ void reverseHalfPyra(int n){
     }
 
 }
+
+void halfPyraNum(int n){
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=i; j++){
+            cout << " " << j << " ";
+        }
+        cout << endl;
+    }
+
+}
 int main(){
      int n;
     cin >> n;
@@ -39,4 +49,7 @@ int main(){
 
     //calling reverse pyramid pattern
     reverseHalfPyra(n);
+
+    //calling half pyramid number pattern function
+    halfPyraNum(n);
 }
